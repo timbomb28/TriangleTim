7a4ec879ac41a7c2e5a028d699efb3708d2cb7b0
timkargl <kargl_tim@web.de>
2025-06-02 22:02:39 +0200

    firstly

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+package triangle;
+
+import resizable.ResizableFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        ResizableFrame triangle = new ResizableFrame(new triangle.Triangle());
+    }
+}
